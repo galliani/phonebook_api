4b64ed1cb432ace55df1e974345ddd89ec366dfa
Galih Muhammad <galih0muhammad@gmail.com>
2017-11-20 20:57:26 +0700

    get non-persisting database and endpoints functions written

--- main.go
+++ main.go
@@ -5,10 +5,56 @@ import (
     "log"
     "net/http"
     "github.com/gorilla/mux"
+    "fmt"
 )
+
+// The Model
+// The omitempty options means that should the field be falsey, then omit it from the response
+type Person struct {
+    ID        string   `json:"id,omitempty"`
+    Firstname string   `json:"firstname,omitempty"`
+    Lastname  string   `json:"lastname,omitempty"`
+    Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+    City  string `json:"city,omitempty"`
+    State string `json:"state,omitempty"`
+}
+
+var people []Person
 
 // our main function
 func main() {
+    // Sets up the router
     router := mux.NewRouter()
-    log.Fatal(http.ListenAndServe(":8000", router))
+
+    people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+    people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+    people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
+    // The endpoints, each calling a function to create the response
+    router.HandleFunc("/people", GetPeople).Methods("GET")
+    router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+    router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+    router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+
+    port := ":8000"
+    fmt.Println("The server has started and is listening")
+
+    listenErr := http.ListenAndServe(port, router)
+    if listenErr != nil {
+      log.Fatal(listenErr)
+    }
+
 }
+
+
+// The functions responsible for generating the response for the endpoints
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+    json.NewEncoder(w).Encode(people)
+}
+
+func GetPerson(w http.ResponseWriter, r *http.Request) {}
+func CreatePerson(w http.ResponseWriter, r *http.Request) {}
+func DeletePerson(w http.ResponseWriter, r *http.Request) {}
+
