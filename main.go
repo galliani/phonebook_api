aaef855711489c0c70712a9e66fedb51aeaa1328
Galih Muhammad <galih0muhammad@gmail.com>
2017-11-21 21:12:06 +0700

    separate models into a new folder and main file

--- main.go
+++ main.go
@@ -6,31 +6,19 @@ import (
     "net/http"
     "github.com/gorilla/mux"
     "fmt"
+    "./models"
 )
 
-// The Model
-// The omitempty options means that should the field be falsey, then omit it from the response
-type Person struct {
-    ID        string   `json:"id,omitempty"`
-    Firstname string   `json:"firstname,omitempty"`
-    Lastname  string   `json:"lastname,omitempty"`
-    Address   *Address `json:"address,omitempty"`
-}
-type Address struct {
-    City  string `json:"city,omitempty"`
-    State string `json:"state,omitempty"`
-}
-
-var people []Person
+var people []models.Person
 
 // our main function
 func main() {
     // Sets up the router
     router := mux.NewRouter()
 
-    people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-    people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-    people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+    people = append(people, models.Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &models.Address{City: "City X", State: "State X"}})
+    people = append(people, models.Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &models.Address{City: "City Z", State: "State Y"}})
+    people = append(people, models.Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
 
     // The endpoints, each calling a function to create the response
     router.HandleFunc("/people", GetPeople).Methods("GET")
@@ -64,13 +52,13 @@ func GetPerson(w http.ResponseWriter, r *http.Request) {
         }
     }
 
-    json.NewEncoder(w).Encode(&Person{})
+    json.NewEncoder(w).Encode(&models.Person{})
 }
 
 func CreatePerson(w http.ResponseWriter, r *http.Request) {
     params := mux.Vars(r)
 
-    var person Person
+    var person models.Person
     _ = json.NewDecoder(r.Body).Decode(&person)
     
     person.ID = params["id"]
---
+++ models/main.go
@@ -0,0 +1,14 @@
+package models
+
+// The Model
+// The omitempty options means that should the field be falsey, then omit it from the response
+type Person struct {
+    ID        string   `json:"id,omitempty"`
+    Firstname string   `json:"firstname,omitempty"`
+    Lastname  string   `json:"lastname,omitempty"`
+    Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+    City  string `json:"city,omitempty"`
+    State string `json:"state,omitempty"`
+}
